65295560d47c9a75cdb53ab2589c9818eb42251c
AlbertOllerThinkNGrow <Albert.Oller@thinkngrow.com>
2017-10-10 13:26:30 +0200

    Add ARC122UBuzzerEnabledCommand class

---
+++ PCSCLib/ARC122U/ARC122UBuzzerEnabledCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCSCLib.ARC122U
+{
+    class ARC122UBuzzerEnabledCommand : ARC122UCommand
+    {
+        protected override byte[] Command { get; set; }
+
+        public ARC122UBuzzerEnabledCommand(bool buzzerEnabled)
+        {
+            Class = 0xFF;
+            Instruction = 0x00;
+            P1 = 0x52;
+            P2 = (buzzerEnabled ? (byte)0xFF : (byte)0x00);
+            LcLe = 0x00;
+
+            Command = new byte[]
+            {
+                Class,
+                Instruction,
+                P1,
+                P2,
+                LcLe
+            };
+        }
+    }
+}
